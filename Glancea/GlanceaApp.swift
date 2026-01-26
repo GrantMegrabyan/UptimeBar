e05e2ccc7d6eccbfff9a526d865e138ac2e7fac8
Grant Megrabyan <grant.megrabyan@gmail.com>
2026-01-26 00:25:42 +0000

    Initial Commit

---
+++ Glancea/GlanceaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceaApp.swift
+//  Glancea
+//
+//  Created by Grant Megrabyan on 26/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct GlanceaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlanceaTests/GlanceaTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceaTests.swift
+//  GlanceaTests
+//
+//  Created by Grant Megrabyan on 26/01/2026.
+//
+
+import Testing
+@testable import Glancea
+
+struct GlanceaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
